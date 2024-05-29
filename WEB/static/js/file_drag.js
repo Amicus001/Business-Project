ec94ccb017509f8a19605405945487bae901f4ab
ParkHeeJin00 <mybae78@gmail.com>
2024-05-29 18:40:33 +0900

    0529

--- WEB/static/js/file_drag.js
+++ WEB/static/js/file_drag.js
@@ -1,105 +1,60 @@
-var input = document.getElementById("input");
-var initLabel = document.getElementsByClassName('upload-box');
+  document.addEventListener('DOMContentLoaded', () => {
+    const dragArea = document.querySelector('.drag-drop-area');
+    const fileInput = document.getElementById('file');
+    const uploadText = document.getElementById('uploadText');
 
-input.addEventListener("change", (event) => {
-  const files = changeEvent(event);
-  handleUpdate(files);
-});
+    if (dragArea) {
+        dragArea.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            dragArea.classList.add('drag-over');
+        });
 
-initLabel.addEventListener("mouseover", (event) => {
-  event.preventDefault();
-  const label = document.getElementById("label");
-  label?.classList.add("label--hover");
-});
+        dragArea.addEventListener('dragleave', (e) => {
+            e.preventDefault();
+            dragArea.classList.remove('drag-over');
+        });
 
-initLabel.addEventListener("mouseout", (event) => {
-  event.preventDefault();
-  const label = document.getElementById("label");
-  label?.classList.remove("label--hover");
-});
+        dragArea.addEventListener('drop', (e) => {
+            e.preventDefault();
+            dragArea.classList.remove('drag-over');
+            const files = e.dataTransfer.files;
+            handleFiles(files);
+            displayFileNames(files);
+        });
 
-document.addEventListener("dragenter", (event) => {
-  event.preventDefault();
-  console.log("dragenter");
-  if (event.target.className === "inner") {
-    event.target.style.background = "#616161";
-  }
-});
+        dragArea.addEventListener('click', (e) => {
+            if (e.target === dragArea || e.target === uploadText) {
+                fileInput.click();
+            }
+        });
+    }
 
-document.addEventListener("dragover", (event) => {
-  console.log("dragover");
-  event.preventDefault();
-});
-
-document.addEventListener("dragleave", (event) => {
-  event.preventDefault();
-  console.log("dragleave");
-  if (event.target.className === "inner") {
-    event.target.style.background = "#3a3a3a";
-  }
-});
-
-document.addEventListener("drop", (event) => {
-  event.preventDefault();
-  console.log("drop");
-  if (event.target.className === "inner") {
-    const files = event.dataTransfer?.files;
-    event.target.style.background = "#3a3a3a";
-    handleUpdate([...files]);
-  }
-});
-
-function changeEvent(event){
-  const { target } = event;
-  return [...target.files];
-};
-
-function handleUpdate(fileList){
-  const preview = document.getElementById("preview");
-
-  fileList.forEach((file) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", (event) => {
-      const img = el("img", {
-        className: "embed-img",
-        src: event.target?.result,
-      });
-      const imgContainer = el("div", { className: "container-img" }, img);
-      preview.append(imgContainer);
+    fileInput.addEventListener('change', (event) => {
+        const files = event.target.files;
+        handleFiles(files);
+        displayFileNames(files);
     });
-    reader.readAsDataURL(file);
-  });
-};
 
-function el(nodeName, attributes, ...children) {
-  const node =
-    nodeName === "fragment"
-      ? document.createDocumentFragment()
-      : document.createElement(nodeName);
-
-  Object.entries(attributes).forEach(([key, value]) => {
-    if (key === "events") {
-      Object.entries(value).forEach(([type, listener]) => {
-        node.addEventListener(type, listener);
-      });
-    } else if (key in node) {
-      try {
-        node[key] = value;
-      } catch (err) {
-        node.setAttribute(key, value);
-      }
-    } else {
-      node.setAttribute(key, value);
+    function handleFiles(files) {
+        for (const file of files) {
+            if (file.type !== 'video/mp4') {
+                alert('Only mp4 files can be uploaded');
+                return;
+            }
+        }
     }
-  });
 
-  children.forEach((childNode) => {
-    if (typeof childNode === "string") {
-      node.appendChild(document.createTextNode(childNode));
-    } else {
-      node.appendChild(childNode);
+    function displayFileNames(files) {
+        let fileNames = [];
+        for (const file of files) {
+            fileNames.push(file.name);
+        }
+        if (fileNames.length > 0) {
+            uploadText.innerHTML = fileNames.join('<br>');
+        } else {
+            uploadText.innerHTML = 'Try again';
+        }
     }
-  });
+});
 
-  return node;
-}
+
--- WEB/views/main_views.py
+++ WEB/views/main_views.py
@@ -3,7 +3,8 @@ import cv2
 import shutil
 import os
 import sys
-
+from flask import Flask, render_template, send_file
+import subprocess
 
 # detect.py 파일이 있는 디렉토리 경로
 # c:\Users\kdp\KDT-5\wk_Project\WEB
@@ -20,12 +21,19 @@ from detect import *
 
 
 
-#bp instance
+# #bp instance
 databp = Blueprint(name='DATA',
                    import_name= __name__,
                    static_folder= 'templates',
                    url_prefix='/input')
 
+# upload 폴더, download 폴더 삭제
+upload_directory = os.path.join(root_dir, 'static', 'upload')
+download_directory = os.path.join(root_dir, 'static', 'download')
+if os.path.exists(upload_directory):
+    shutil.rmtree(upload_directory)
+if os.path.exists(download_directory):
+    shutil.rmtree(download_directory)
 
 # 라우팅
 @databp.route('/') #http://127.0.0.1:5000/input
@@ -33,53 +41,45 @@ def input_data():
     return  render_template('input.html')
 
 
+@databp.route('/upload/loading/')
+def loading():
+    return render_template('loading.html')
+
 
 @databp.route('/upload/', methods=['POST'])
 def upload_file():
 
     # 저장할 디렉토리
     upload_directory = os.path.join(root_dir, 'static', 'upload')
-    if not os.path.exists(upload_directory):
-        os.makedirs(upload_directory)
-
-    if 'file' not in request.files:
-        flash('파일 부분이 없습니다')
-        return render_template('input.html')
+    
+    # 디렉토리 다시 생성
+    os.makedirs(upload_directory)
     
     file = request.files['file']
-    if file.filename == '':
-        flash('선택된 파일이 없습니다')
-        return render_template('input.html')
-    
-    if file and allowed_file(file.filename):
-        # 업로드된 파일의 이름을 유지하고 지정된 디렉토리에 저장
-        file_path = os.path.join(upload_directory, 'upload_vedio.mp4')
-        # 파일 존재하면 삭제
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            
-        file.save(file_path)
+    file_path = os.path.join(upload_directory, 'video.mp4')
 
-        # YOLO 모델 실행 및 결과 파일 경로 가져오기
-        result_path = run_yolo_detection(file_path)
-        
-        # 결과 파일 경로를 URL에 인코딩하여 리다이렉트
-        return redirect(url_for('output', result=result_path))
-    else:
-        flash('mp4 파일만 허용됩니다')
-        return render_template('input.html')
+    file.save(file_path)
+    # YOLO 모델 실행 및 결과 파일 경로 가져오기
+    result_path = run_yolo_detection(file_path) 
+    # 결과 파일 경로를 URL에 인코딩하여 리다이렉트
+    return redirect(url_for('DATA.output', result=result_path))
 
-# mp4 형식만 허용하는 함수
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'mp4'}
+# 비디오 인코딩  
+def encode_video(input_path, output_path):
+    command = [
+        'ffmpeg', '-y', '-i', input_path,
+        '-c:v', 'libx264', '-c:a', 'aac', '-strict', 'experimental',
+        output_path
+    ]
+    subprocess.run(command, check=True)
 
 # YOLO 모델 실행
 def run_yolo_detection(file_path):
     name = 'de_id'
-
-    download_directory = os.path.join(root_dir, 'static', 'download')
-    if not os.path.exists(download_directory):
-        os.makedirs(download_directory)
+    # YOLO 결과 파일 경로: 파일 존재하면 삭제
+    download_directory = os.path.join(root_dir, 'static', 'model', 'yolov5', 'runs', 'detect', name)
+    if os.path.exists(download_directory):
+        shutil.rmtree(download_directory)
 
     # YOLOv5 모델 실행
     run(
@@ -93,25 +93,39 @@ def run_yolo_detection(file_path):
         conf_thres=0.5,
     )
 
-    # 결과 파일 경로 반환
-    result_filename = f"{name}.mp4"
-    result_path = os.path.join(download_directory, result_filename)
-    return result_path
+    save_path = os.path.join(root_dir, 'static', 'model', 'yolov5', 'runs', 'detect', name, 'video.mp4')
+
+    # YOLO 결과 파일 경로
+    download_path = os.path.join(root_dir, 'static', 'download', 'de_video.mp4')
+    # download 폴더 생성
+    os.makedirs(os.path.dirname(download_path), exist_ok=True)
+
+    # 비식별화 비디오 인코딩
+    encode_video(save_path, download_path)
+
+    return download_path
 
 
-@databp.route('/output/', methods=['GET'])
+
+
+
+
+@databp.route('/result/', methods=['GET'])
 def output():
     result_path = request.args.get('result')
-
-    if result_path:
-        # 결과 동영상 파일을 다운로드할 수 있도록 함
-        return render_template('output.html', result=result_url)
+    if result_path and os.path.exists(result_path):
+        # 파일 다운로드 링크를 생성하여 템플릿에 전달
+        return render_template('result.html', result=result_path)
     else:
-        return  flash('결과를 찾을 수 없습니다')
+        flash('결과를 찾을 수 없습니다')
+        return render_template('result.html', result=None)
 
-
-
-
+@databp.route('/download/<path:filename>', methods=['GET'])
+def download(filename):
+    # 다운로드할 파일 경로
+    file_path = os.path.join(root_dir, 'static', 'download', filename)
+    # 파일 다운로드
+    return send_file(file_path, as_attachment=True)
 
 
 
